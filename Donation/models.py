bc3ae2bddb2e2edd541c58961af98ba9a2b15f9c
Brian Kariuki Wachira <73661988+wachira-brian@users.noreply.github.com>
2025-04-23 19:00:05 +0300

    Delete Donation directory

--- Donation/models.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-class Donation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), default='Anonymous')
-    amount = db.Column(db.Float, nullable=False)
